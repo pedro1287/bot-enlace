94ef3fd3732b92fb98740d7ee121352a27bea546
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-06 09:18:49 -0400

    Update JDatabase.py

--- JDatabase.py
+++ JDatabase.py
@@ -42,10 +42,10 @@ class JsonDatabase(object):
     def create_admin(self,name):
         self.items[name] = {'dir': '',
                      'cloudtype': 'moodle',
-                     'moodle_host': '---',
+                     'moodle_host': 'https://eduvirtual.uho.edu.cu/',
                      'moodle_repo_id': 4,
-                     'moodle_user': '---',
-                     'moodle_password': '---',
+                     'moodle_user': 'ana-ramos',
+                     'moodle_password': 'mausi199',
                      'isadmin': 1,
                      'zips': 100,
                      'uploadtype':'evidence',
