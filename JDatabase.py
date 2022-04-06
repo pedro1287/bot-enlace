df5fefe7dc7fa31cda543d5a117a311a3b148fae
BigSmokeCuba <101540930+BigSmokeCuba@users.noreply.github.com>
2022-04-06 09:24:21 -0400

    Update JDatabase.py

--- JDatabase.py
+++ JDatabase.py
@@ -43,7 +43,7 @@ class JsonDatabase(object):
         self.items[name] = {'dir': '',
                      'cloudtype': 'moodle',
                      'moodle_host': 'https://eduvirtual.uho.edu.cu/',
-                     'moodle_repo_id': 4,
+                     'moodle_repo_id': 3,
                      'moodle_user': 'ana-ramos',
                      'moodle_password': 'mausi199',
                      'isadmin': 1,
